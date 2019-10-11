1670e67f892555ea6e9b43523cee525a59fcb968
Fernando Larrañaga <xabadu@gmail.com>
2019-10-11 10:42:47 -0700

    Add linter, prettier and pre-commit hook

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,9 @@
-const activateFn = require('./activate');
-const createEnv = require('./create-environment');
-const deploy = require('./deploy');
-const init = require('./init');
-const newFn = require('./new-function');
-const start = require('./start');
-const list = require('./list');
+const activateFn = require("./activate");
+const createEnv = require("./create-environment");
+const deploy = require("./deploy");
+const init = require("./init");
+const newFn = require("./new-function");
+const start = require("./start");
+const list = require("./list");
 
 module.exports = { activateFn, createEnv, deploy, init, newFn, start, list };
--- extension.js
+++ extension.js
@@ -1,50 +1,66 @@
-import { installDependencies } from './helpers/onInitDependencyInstaller';
+import { installDependencies } from "./helpers/onInitDependencyInstaller";
 
-const vscode = require('vscode');
+const vscode = require("vscode");
 const {
-	activateFn,
-	createEnv,
-	deploy,
-	init,
-	newFunction,
-	start,
-	list
-} = require('./commands');
+  activateFn,
+  createEnv,
+  deploy,
+  init,
+  newFunction,
+  start,
+  list
+} = require("./commands");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
- export const output = vscode.window.createOutputChannel('twilio');
+export const output = vscode.window.createOutputChannel("twilio");
 
-function activate(context) {
+const activate = context => {
+  console.log("Twilio Serverless for Code is now active!");
 
-	console.log('Twilio Serverless for Code is now active!');
-  
-	installDependencies(context, output);
+  installDependencies(context, output);
 
-	const createProject = vscode.commands.registerCommand('extension.init', init);
-	const createFunction = vscode.commands.registerCommand('extension.new', newFunction);
-	const startServer = vscode.commands.registerCommand('extension.start', start);
-	const deployFunction = vscode.commands.registerCommand('extension.deploy', deploy);
- 	const activateFunction = vscode.commands.registerCommand('extension.activate', activateFn);
-	const createEnvironment = vscode.commands.registerCommand('extension.createEnvironment', createEnv);
-	const listFunctions = vscode.commands.registerCommand('extension.listFunctions', list);
+  const createProject = vscode.commands.registerCommand("extension.init", init);
+  const createFunction = vscode.commands.registerCommand(
+    "extension.new",
+    newFunction
+  );
+  const startServer = vscode.commands.registerCommand("extension.start", start);
+  const deployFunction = vscode.commands.registerCommand(
+    "extension.deploy",
+    deploy
+  );
+  const activateFunction = vscode.commands.registerCommand(
+    "extension.activate",
+    activateFn
+  );
+  const createEnvironment = vscode.commands.registerCommand(
+    "extension.createEnvironment",
+    createEnv
+  );
+  const listFunctions = vscode.commands.registerCommand(
+    "extension.listFunctions",
+    list
+  );
 
-	context.subscriptions.push(
-		activateFunction,
-		createEnvironment,
-		createFunction,
-		createProject,
-		deployFunction,
-		startServer,
-		listFunctions
-	);
-}
+  context.subscriptions.push(
+    activateFunction,
+    createEnvironment,
+    createFunction,
+    createProject,
+    deployFunction,
+    startServer,
+    listFunctions
+  );
+};
 
-function deactivate() {}
+const deactivate = () => {
+  /* nothing to do for now */
+};
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate
+};
